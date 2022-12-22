d360d94720caa52092bc577b66cb625a2fe9102a
- <->
2022-12-22 03:01:12 +0100

    fix problems

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,14 +1,9 @@
 import { Command } from '../types'
-import config from '../config'
 
 export default {
   name: 'help',
   aliases: ['h', 'cmd', 'command'],
   run: async (client, message) => {
-    message.channel.send(
-      `${config.emoji.error} | You must be in a voice channel or enter a voice channel id!`
-    )
-
     message.channel.send({
       embeds: [
         {
